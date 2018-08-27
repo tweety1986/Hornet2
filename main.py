31d5090e885455d8404d3173ba357bdb94d8f0ac
tweety1986 <thuy05061986@gmail.com>
2018-08-27 16:38:42 +0200

    Add logoout function and sesion live

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import hashlib
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=2)
 
 
 def check_password(hashed_password, user_password):
@@ -58,6 +58,11 @@ def login():
 
     return render_template('login.html', error=error)
 
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=0)
+    return redirect(url_for('index'))
 
 @app.route('/child')
 def child():
@@ -69,11 +74,11 @@ def child():
 
 @app.route('/secret')
 def secret():
-    if not session.get('logged_in'):
+    if not session.get('loged_in'):
         return redirect(url_for('login'))
     else:
         return "Hello Boss!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8050)
+    app.run(host='0.0.0.0', debug=True, port=8060)
